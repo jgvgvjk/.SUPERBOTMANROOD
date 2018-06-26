469a9bc98d803ec5d79a397f8f2bd1369ac0f445
Modo <39749105+ImHamoood@users.noreply.github.com>
2018-06-26 13:02:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1347,7 +1347,7 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('---superbc')
- if(!message.author.id === '411911292643704854') return;
+ if(!message.author.id === '371696327005831169') return;
 message.channel.sendMessage(' جار ارسال الرسالة | ✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
