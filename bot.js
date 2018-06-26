8c790887b2872ae376d7da4364fd7deb2af38a3f
Modo <39749105+ImHamoood@users.noreply.github.com>
2018-06-26 14:56:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1372,6 +1372,23 @@ omar.reply("`تم حذف جميع الرتب بنجاح`")
 });
 
 
+var prefix = "-"
+client.on('message', message => {
+
+  if (message.content.startsWith( prefix + "sug")) {
+  if (!message.channel.guild) return;
+  let args = message.content.split(" ").slice(1).join(' ');
+  client.channels.get("ايدي الروم").send(
+      "\n" + "**" + "● السيرفر :" + "**" +
+      "\n" + "**" + "» " + message.guild.name + "**" +
+      "\n" + "**" + " ● المرسل : " + "**" +
+      "\n" + "**" + "» " + message.author.tag + "**" +
+      "\n" + "**" + " ● الرسالة : " + "**" +
+      "\n" + "**" + args + "**")
+  }
+  });
+ 
+
 
 
 
